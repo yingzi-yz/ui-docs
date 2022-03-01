3587e213d07592175a942b7ebd895ed5cb8822e3
luweixin <1026991752@qq.com>
2022-03-01 09:23:26 +0800

    0.2.1版本文档

--- src/views/update.vue
+++ src/views/update.vue
@@ -2,6 +2,10 @@
   <div class="update-wrap">
     <yz-tip>更新日期</yz-tip>
     <yz-collapse v-model="versionData">
+      <yz-collapse-item title="0.2.1版本" name="4">
+        <div>发布时间：2022-03-01</div>
+        <div>修改inputNum组件双向绑定值</div>
+      </yz-collapse-item>      
       <yz-collapse-item title="0.2.0版本" name="3">
         <div>发布时间：2022-02-14</div>
         <div>增加若干组件</div>
@@ -23,7 +27,7 @@ export default {
   name: 'updateWrap',
   data() {
     return {
-      versionData: ['3']
+      versionData: ['4']
     }
   }
 }
